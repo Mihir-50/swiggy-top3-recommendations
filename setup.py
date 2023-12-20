2e0fed56883d56561a694d4f42c7d6d74ea2653d
mihir <mihir231216@gmail.com>
2023-12-20 10:33:03 +0530

    Cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Project will help to recommend top3 food items on Swiggy home page',
+    author='Mihir Srivastava',
+    license='',
+)
